cfac96de5077f6e711cc15c334fe0547325739d7
chakrisatish <40446487+chakrisatish84@users.noreply.github.com>
2018-10-18 00:46:53 -0700

    Dynamic Programing

--- DSandProblemSolving/DSandProblemSolving/Program.cs
+++ DSandProblemSolving/DSandProblemSolving/Program.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace DSandProblemSolving
 {
+    class Job : IComparable
+    {
+        public int start;
+        public int end;
+        public int profit;
+        public Job(int start, int end, int profit)
+        {
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Job)
+            {
+                return this.end.CompareTo((obj as Job).end);  // compare user names
+            }
+            throw new ArgumentException("Object is not a User");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -151,6 +172,45 @@ namespace DSandProblemSolving
             //int[] swapArray = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
             //SwapzeroOneTwo(swapArray);
 
+            //Minimunm Jumps to reach end of the array.
+            //int[] array  = { 2,1,3,2,3,4,5,1,2,8 };
+            //int[] JumpPathArray = new int[array.Length]; // to store where it jumps from.
+            //int[] JumpArray = new int[array.Length]; // to store minimum number requries for each index.
+
+
+            //String str1 = "ABCDGHLQR";
+            //String str2 = "AEDPHR";
+
+            //lcsDynamic(str1.ToCharArray(), str2.ToCharArray());
+
+            //int[] array = { 2, 3, 7, 8, 10 };
+            //subsetSumArray(array, 10); // need subset where sum is 11
+
+            //int[] array = { 4, 2, 3, 5, 3 };
+            //MatrixMultiplicationCost(array);
+
+            //int[] array = { 3, 2, 4 };
+            //coinChangeCombination(array, 6); // minimumn coins needed to make the needed total here 6.
+
+            //Another way two dimensioianl
+            //int[] coinsArray = { 1, 5, 6, 8 };
+            //coinChangeCombination2dway(coinsArray, 11);
+
+            //String str1 = "azced";
+            //String str2 = "abcdef";           
+            //dynamicEditDistance(str1.ToCharArray(), str2.ToCharArray());
+
+            //WeightedJobScheduledProblem
+            Job[] jobs = new Job[6];
+            jobs[0] = new Job(1, 3, 5);
+            jobs[1] = new Job(2, 5, 6);
+            jobs[2] = new Job(4, 6, 5);
+            jobs[3] = new Job(6, 7, 4);
+            jobs[4] = new Job(7, 9, 2);
+            jobs[5] = new Job(5, 8, 11);
+
+            maximumProfits(jobs);
+
             int[,] matrix = new int[4, 4]{
             {1,2,3,4},
             {5,6,7,8},
@@ -169,26 +229,330 @@ namespace DSandProblemSolving
             //PrintDiagonal(matrix, matrix.GetUpperBound(0), matrix.GetUpperBound(1));
             //countNegativeValuesinMatrix(Negativematrix, Negativematrix.GetUpperBound(0), Negativematrix.GetUpperBound(1));
             //searchElementinaMatrix(matrix, matrix.GetUpperBound(0), matrix.GetUpperBound(1), 25);
-            PrintAllDiagonal(matrix, matrix.GetUpperBound(0), matrix.GetUpperBound(1));
+            //PrintAllDiagonal(matrix, matrix.GetUpperBound(0), matrix.GetUpperBound(1));
 
         }
 
+        private static void maximumProfits(Job[] jobs)
+        {
+            int[] temp = new int[jobs.Length];
+            Array.Sort(jobs); // Since we have implemented Icomparable to job class and implemented compareto function
+
+            temp[0] = jobs[0].profit;
+            for (int i = 1; i < jobs.Length; i++)
+            {
+                temp[i] = Math.Max(jobs[i].profit, temp[i - 1]);
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (jobs[j].end <= jobs[i].start)
+                    {
+                        temp[i] = Math.Max(temp[i], jobs[i].profit + temp[j]);
+                        break;
+                    }
+                }
+            }           
+            Console.WriteLine(temp.Max());
+        }
+
+        private static void dynamicEditDistance(char[] string1, char[] string2)
+        {
+            int[,] temp = new int[string1.Length + 1, string2.Length + 1];
+
+            for (int i = 0; i < string1.Length; i++)
+            {
+                temp[i, 0] = i;
+            }
+            for (int i = 0; i < string2.Length; i++)
+            {
+                temp[0, i] = i;
+            }
+
+            for (int i = 1; i <= string1.Length; i++)
+            {
+                for (int j = 1; j <= string2.Length; j++)
+                {
+                    if (string1[i - 1] == string2[j - 1])
+                    {
+                        temp[i, j] = temp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        temp[i, j] = min(temp[i - 1, j], temp[i, j - 1], temp[i - 1, j - 1]) + 1;
+                    }
+                }
+            }
+            printActualEdits(temp, string1, string2);
+            Console.WriteLine(temp[string1.Length, string2.Length]);
+        }
+        public static void printActualEdits(int[,] T, char[] str1, char[] str2)
+        {
+            int i = T.Length - 1;
+            int j = T.Length - 1;
+            while (true)
+            {
+                if (i == 0 || j == 0)
+                {
+                    break;
+                }
+                if (str1[i - 1] == str2[j - 1])
+                {
+                    i = i - 1;
+                    j = j - 1;
+                }
+                else if (T[i, j] == T[i - 1, j - 1] + 1)
+                {
+                    Console.WriteLine("Edit " + str2[j - 1] + " in string2 to " + str1[i - 1] + " in string1");
+                    i = i - 1;
+                    j = j - 1;
+                }
+                else if (T[i, j] == T[i - 1, j] + 1)
+                {
+                    Console.WriteLine("Delete in string1 " + str1[i - 1]);
+                    i = i - 1;
+                }
+                else if (T[i, j] == T[i, j - 1] + 1)
+                {
+                    Console.WriteLine("Delete in string2 " + str2[j - 1]);
+                    j = j - 1;
+                }
+            }
+        }
+        private static int min(int a, int b, int c)
+        {
+            int l = Math.Min(a, b);
+            return Math.Min(l, c);
+        }
+
+        private static void coinChangeCombination2dway(int[] array, int total)
+        {
+            int[,] temp = new int[array.Length + 1, total + 1];
+            for (int i = 0; i <= array.Length; i++)
+            {
+                temp[i, 0] = 0;
+            }
+
+            for (int i = 1; i <= total; i++)
+            {
+                temp[0, i] = i;
+            }
+
+            for (int i = 1; i <= array.Length; i++)
+            {
+                for (int j = 1; j <= total; j++)
+                {
+                    if (j >= array[i - 1])
+                    {
+                        temp[i, j] = Math.Min(temp[i - 1, j], temp[i, j - array[i - 1]] + 1);
+                    }
+                    else
+                    {
+                        temp[i, j] = temp[i - 1, j];
+                    }
+                }
+            }
+            Console.WriteLine(temp[array.Length, total]);
+
+            int arrayLength = array.Length;
+
+            do
+            {
+                if (temp[arrayLength, total] != 0 && temp[arrayLength, total] == temp[arrayLength - 1, total])
+                {
+                    arrayLength = arrayLength - 1;
+                }
+                else
+                {
+                    Console.WriteLine(array[arrayLength - 1]);
+                    total = total - array[arrayLength - 1];
+                    arrayLength = arrayLength - 1;
+                }
+            }
+            while (total != 0);
+        }
+
+        private static void coinChangeCombination(int[] array, int total)
+        {
+            //Make two arrays with total +1 length
+            int[] temp1 = new int[total + 1];
+            int[] temp2 = new int[total + 1];
+
+            for (int i = 0; i <= total; i++)
+            {
+                temp1[i] = int.MaxValue - 1;
+                temp2[i] = -1;
+            }
+            temp1[0] = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j <= total; j++)
+                {
+                    if (j >= array[i])
+                    {
+                        if (temp1[j] > temp1[j - array[i]] + 1)
+                        {
+                            temp2[j] = i;
+                            temp1[j] = temp1[j - array[i]] + 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Result :" + temp1[temp1.Length - 1]);
+            int maxCoins = temp1[temp1.Length - 1];
+            while (maxCoins != 0 && maxCoins != int.MaxValue - 1)
+            {
+                Console.WriteLine(array[maxCoins]);
+                maxCoins = temp1[maxCoins];
+            }
+        }
+        private static void minimunJumps(int[] array, int[] jumpArray, int[] jumpPathArray)
+        {
+            jumpArray[0] = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                jumpArray[i] = int.MaxValue - 1;
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] + j >= i)
+                    {
+                        if (jumpArray[j] + 1 < jumpArray[i])
+                        {
+                            jumpPathArray[i] = j;
+                            jumpArray[i] = jumpArray[j] + 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(jumpArray[jumpArray.Length - 1]);
+
+            int minimumJump = jumpPathArray.Length - 1;
+            while (minimumJump != 0)
+            {
+                Console.WriteLine(minimumJump);
+                minimumJump = jumpPathArray[minimumJump];
+            }
+            Console.WriteLine("0");
+        }
+        private static void MatrixMultiplicationCost(int[] arr)
+        {
+            int[,] temp = new int[arr.Length, arr.Length];
+            int q = 0;
+            for (int l = 2; l < arr.Length; l++)
+            {
+                for (int i = 0; i < arr.Length - l; i++)
+                {
+                    int j = i + l;
+                    temp[i, j] = 1000000;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        q = temp[i, k] + temp[k, j] + arr[i] * arr[k] * arr[j];
+                        if (q < temp[i, j])
+                        {
+                            temp[i, j] = q;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(temp[0, arr.Length - 1]);
+        }
+
+        private static void subsetSumArray(int[] array, int sunValue)
+        {
+            bool[,] temp = new bool[array.Length + 1, sunValue + 1];
+            for (int i = 0; i < array.Length + 1; i++)
+            {
+                temp[i, 0] = true;
+            }
+
+            for (int j = 1; j <= sunValue; j++)
+            {
+                temp[0, j] = false;
+            }
+
+            for (int i = 1; i <= array.Length; i++)
+            {
+                for (int j = 1; j <= sunValue; j++)
+                {
+                    if (j < array[i - 1])
+                    {
+                        temp[i, j] = temp[i - 1, j];
+                    }
+                    else
+                    {
+                        temp[i, j] = temp[i - 1, j] || temp[i - 1, j - array[i - 1]];
+                    }
+                }
+            }
+            //Write the sub set element where sum of those values is the expected sum value
+            int arrayLength = array.Length;
+            do
+            {
+                if (temp[arrayLength, sunValue])
+                {
+                    arrayLength = arrayLength - 1;
+                }
+                else
+                {
+                    Console.WriteLine(array[arrayLength]);
+                    sunValue = sunValue - array[arrayLength];
+                    arrayLength = arrayLength - 1;
+
+
+                }
+
+            }
+            while (sunValue != 0);
+            Console.WriteLine(temp[array.Length, sunValue]);
+        }
+
+        private static void lcsDynamic(char[] str1, char[] str2)
+        {
+            int[,] temp = new int[str1.Length + 1, str2.Length + 1];
+            int max = 0;
+            for (int i = 1; i < str1.Length + 1; i++)
+            {
+                for (int j = 1; j < str2.Length + 1; j++)
+                {
+                    if (str1[i - 1] == str2[j - 1])
+                    {
+                        temp[i, j] = temp[i - 1, j - 1] + 1;
+                        Console.WriteLine(str1[i - 1]);
+                    }
+                    else
+                    {
+                        temp[i, j] = Math.Max(temp[i, j - 1], temp[i - 1, j]);
+                    }
+                    if (temp[i, j] > max)
+                    {
+                        max = temp[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+
+
         private static void PrintAllDiagonal(int[,] matrix, int lastRow, int lastCol)
         {
-          for(int i=0; i <= lastRow; i++)
+            for (int i = 0; i <= lastRow; i++)
             {
                 int j = i, k = 0;
                 while (j >= 0)
                 {
-                    Console.WriteLine(matrix[j,k]);
+                    Console.WriteLine(matrix[j, k]);
                     j--;
                     k++;
                 }
             }
-          for(int i=1; i<=lastCol; i++)
+            for (int i = 1; i <= lastCol; i++)
             {
                 int j = lastRow, k = i;
-                while (j >=i && k<=lastCol)
+                while (j >= i && k <= lastCol)
                 {
                     Console.WriteLine(matrix[j, k]);
                     j--;
@@ -202,7 +566,8 @@ namespace DSandProblemSolving
             int i = 0, j = lastCol;
             bool searchElementFound = false;
 
-            while (i <= lastRow && j >= 0) {
+            while (i <= lastRow && j >= 0)
+            {
 
                 if (matrix[i, j] == searchElement)
                 {
@@ -231,21 +596,21 @@ namespace DSandProblemSolving
         private static void countNegativeValuesinMatrix(int[,] negativematrix, int lastRow, int lastCol)
         {
             int count = 0;
-            for(int i=0; i<= lastRow; i++)
+            for (int i = 0; i <= lastRow; i++)
             {
                 int j = lastCol;
-                while(j >= 0)
+                while (j >= 0)
                 {
-                    if(negativematrix[i,j] < 0)
+                    if (negativematrix[i, j] < 0)
                     {
-                        count += j+1;
+                        count += j + 1;
                         break;
                     }
                     j--;
                 }
             }
 
-            Console.WriteLine("Negative values count in this matrix "+ count);
+            Console.WriteLine("Negative values count in this matrix " + count);
         }
 
         private static void PrintDiagonal(int[,] matrix, int lastRow, int lastCol)
